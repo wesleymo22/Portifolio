87b64c2bd1bba1e431af0a61e3766fa792d2f978
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-01-30 17:40:38 -0300

    sobre

--- portwes/src/Componentes/Sobre/Index.js
+++ portwes/src/Componentes/Sobre/Index.js
@@ -1,13 +1,20 @@
 import './Sobre.css'
+import {BsPersonCircle} from 'react-icons/bs'
 
 const Sobre = () => {
-    return(
+    return (
         <section id='sobre'>
-            <h2>Sobre</h2>
-            <p><b>Ola, Me chamo Wesley</b>, Apaixonado por tecnologia e desde criança gosto de descobrir o que há de mais novo no ramo de tecnologia</p>
-            <p>Atuo na parte de Desenvolvimento Front-End desde 2022, Formado em Sistemas de Informaçao, possuo dominio nas seguintes linguagens <b>HTML 5, CSS 3, JavaScript e ReactJS</b></p>
-            <p>Clique no botao abaixo para saber mais</p>
-            <a href="https://www.linkedin.com/in/wesleymo22/">Link</a>
+            <div className='imagem'>
+                <BsPersonCircle className='icone'/>
+            </div>
+            <div className='conteudo'>
+                <h2>Sobre</h2>
+                <p><b>Ola, Me chamo Wesley</b>, Apaixonado por tecnologia e desde criança gosto de descobrir o que há de mais novo no ramo de tecnologia</p>
+                <p>Atuo na parte de Desenvolvimento Front-End desde 2022, Formado em Sistemas de Informaçao, possuo dominio nas seguintes linguagens <b>HTML 5, CSS 3, JavaScript e ReactJS</b></p>
+                <p>Clique no botao abaixo para saber mais</p>
+                <a href="https://www.linkedin.com/in/wesleymo22/">Link</a>
+            </div>
+
         </section>
     )
 }
