6007e56ae1bfed16f98a2c151e1d308b6e796c1e
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-01-31 16:07:46 -0300

    Contato teste

--- portwes/src/Componentes/Contato/Index.js
+++ portwes/src/Componentes/Contato/Index.js
@@ -4,7 +4,21 @@ import './Contato.css'
 const Contato = () => {
   return (
     <section id='contato'>
-          <h2>Contato</h2>
+      <div className='dados'>
+        <h2>Contato</h2>
+        <h3>Telefone</h3>
+        <p>(11) 99739-7199</p>
+        <h3>Endereço</h3>
+        <p>R Ibipitanga 922, Pq Continental 3 Guaruhos SP</p>
+        <h3>Email</h3>
+        <p>wesleymo22@gmail.com</p>
+        <h3>Redes Sociais</h3>
+        <p></p>
+
+      </div>
+      <div className='formulario'>
+
+      </div>
     </section>
 
   );
