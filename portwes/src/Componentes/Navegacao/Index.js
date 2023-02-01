e1e0c1aa8f563e498377b1c62d620f723ceb4917
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-02-01 12:42:38 -0300

    Corrigindo navbar

--- portwes/src/Componentes/Navegacao/Index.js
+++ portwes/src/Componentes/Navegacao/Index.js
@@ -1,19 +1,22 @@
 import './Navegacao.css'
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import { useState } from 'react';
 
 const Navegacao = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className='fixed-top'>
+    <Navbar bg="dark" variant="dark" expand="lg" className='fixed-top' expanded={expanded}>
       <Container>
         <Navbar.Brand href="#inicio">Wesley Moraes</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#sobre">Sobre</Nav.Link>
-            <Nav.Link href="#habilidades">Habilidades</Nav.Link>
-            <Nav.Link href="#projetos">Projetos</Nav.Link>
-            <Nav.Link href="#contato">Contato</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} href="#inicio">Inicio</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} href="#sobre">Sobre</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} href="#habilidades">Habilidades</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} href="#projetos">Projetos</Nav.Link>
+            <Nav.Link onClick={() => setExpanded(false)} href="#contato">Contato</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
