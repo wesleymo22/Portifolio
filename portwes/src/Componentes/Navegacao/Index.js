eb7334222ede7bd3b49b110589813b73aed3287c
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-02-01 21:38:42 -0300

    Navbar fontes

--- portwes/src/Componentes/Navegacao/Index.js
+++ portwes/src/Componentes/Navegacao/Index.js
@@ -6,17 +6,17 @@ const Navegacao = () => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className='fixed-top' expanded={expanded}>
+    <Navbar variant="dark" expand="lg" className='fixed-top' expanded={expanded}>
       <Container>
-        <Navbar.Brand href="#inicio">Wesley Moraes</Navbar.Brand>
+        <Navbar.Brand href="#inicio" className='nome'>Wesley Moraes</Navbar.Brand>
         <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link onClick={() => setExpanded(false)} href="#inicio">Inicio</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#sobre">Sobre</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#habilidades">Habilidades</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#projetos">Projetos</Nav.Link>
-            <Nav.Link onClick={() => setExpanded(false)} href="#contato">Contato</Nav.Link>
+            <Nav.Link className='links' onClick={() => setExpanded(false)} href="#inicio">Inicio</Nav.Link>
+            <Nav.Link className='links' onClick={() => setExpanded(false)} href="#sobre">Sobre</Nav.Link>
+            <Nav.Link className='links' onClick={() => setExpanded(false)} href="#habilidades">Habilidades</Nav.Link>
+            <Nav.Link className='links' onClick={() => setExpanded(false)} href="#projetos">Projetos</Nav.Link>
+            <Nav.Link className='links' onClick={() => setExpanded(false)} href="#contato">Contato</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
