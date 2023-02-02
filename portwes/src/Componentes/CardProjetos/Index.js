66abe8e6d0d8cb6a52b04da32424ccc1439691b4
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2023-02-01 21:29:27 -0300

    Ajustes de fonte e cor

--- portwes/src/Componentes/CardProjetos/Index.js
+++ portwes/src/Componentes/CardProjetos/Index.js
@@ -1,17 +1,20 @@
 import './CardProjetos.css'
 import { AiFillGithub } from "react-icons/ai"
 
-const CardProjetos = ({id, titulo, descricao, tecnologias, imagem, git, link}) => {
+const CardProjetos = ({ id, titulo, descricao, tecnologias, imagem, git, link }) => {
     return (
         <div className='cardprojetos' key={id}>
             <div className="cabecalho">
                 <a href={link}><img src={imagem} alt={titulo} /></a>
-                
+
             </div>
             <div className="rodape">
                 <a href={link}><h3>{titulo}</h3></a>
-                <a href={git} className="git-link"><AiFillGithub className='git'/>Github link</a>
+                <a href={git} className="git-link"><AiFillGithub className='git' />Github link</a>
                 <h4>{descricao}</h4>
+
+            </div>
+            <div className='tecnologia'>
                 <h5>{tecnologias}</h5>
             </div>
         </div>
--- portwes/src/Componentes/Sobre/Index.js
+++ portwes/src/Componentes/Sobre/Index.js
@@ -8,8 +8,8 @@ const Sobre = () => {
             </div>
             <div className='conteudo'>
                 <h2>Sobre</h2>
-                <p><b>Ola, Me chamo Wesley</b>, Apaixonado por tecnologia e desde criança gosto de descobrir o que há de mais novo no ramo de tecnologia</p>
-                <p>Atuo na parte de Desenvolvimento Front-End desde 2022, Formado em Sistemas de Informaçao, possuo dominio nas seguintes linguagens <b>HTML 5, CSS 3, JavaScript e ReactJS</b></p>
+                <h4><b>Ola, Me chamo Wesley</b>, Apaixonado por tecnologia e desde criança gosto de descobrir o que há de mais novo no ramo de tecnologia</h4>
+                <h4>Atuo na parte de Desenvolvimento Front-End desde 2022, Formado em Sistemas de Informaçao, possuo dominio nas seguintes linguagens <b>HTML 5, CSS 3, JavaScript e ReactJS</b></h4>
             </div>
         </section>
     )
